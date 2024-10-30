886d204f4ec23551737db1a71da2e02b3a52d2ee
KeviinDavis <daviskevin1@hotmail.com>
2024-10-30 11:34:27 -0400

    Sign-up logic complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
+const User = require('../models/user');
 
 // Render Sign-up page
 router.get('/sign-up', (req, res) => {
   res.render('auth/sign-up');
 });
+
+//Handle sign-up form submission (POST)
+router.post('/sign-up', async (req, res) => {
+try {
+// Checks if username is already taken
+  const existingUser = await User.findOne({ username: req.body.username });
+if (existingUser) {
+  return res.send('Username already taken'); //Feedback if username is taken
+}  
+
+// Hash password
+const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+//Create a new userwith hashed password
+const newUser = new User ({
+  username: req.body.username,
+  password: hashedPassword
+});
+
+// Save the user to the database
+await newUser.save();
+res.redirect('/auth/sign-in'); // Redirect to Sign-in page upon successful sign-up
+} catch (error) {
+console.error(error);
+res.redirect('/auth/sign-up'); // Redirect back if there's an error
+}
+});
+
 
 //Render Sign-in page
 router.get('/sign-in', (req, res) => {
