2f7480166e0b98f3691e105964c69c95fd2773a6
KeviinDavis <daviskevin1@hotmail.com>
2024-11-07 11:32:02 -0500

    AUTH to auth for heroku

--- server.js
+++ server.js
@@ -55,7 +55,10 @@ app.get('/', async (req, res) => {
     res.render('pantry/index', { user: req.session.user || null, items });
 });
 
-  
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+  });  
 
 // Start the server
 const port = process.env.PORT || 3000;
